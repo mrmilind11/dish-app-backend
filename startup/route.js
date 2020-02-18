1809b17dc93ec30e3470b07cc310ce7a03a285cc
Milind Sharma <milind.sharma@searce.com>
2020-02-18 22:11:32 +0530

    base route handled

--- startup/route.js
+++ startup/route.js
@@ -1,4 +1,7 @@
 const CategoryRoute = require('../routes/category.route')
 module.exports = function (app) {
+    app.use('/', (req, res, next) => {
+        res.send('Dish app')
+    })
     app.use('/category', CategoryRoute);
 }
